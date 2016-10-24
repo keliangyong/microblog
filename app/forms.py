62c8972f1129b2e0bd8c5dbec39a2de31c11f051
keliangyong <337215966@qq.com>
2016-10-24 15:36:57 +0800

    form

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python35
+#-*- coding:utf-8 –*-
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python35
 #-*- coding:utf-8 –*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,17 @@ def index():
     title = 'Home',
     posts = posts,
     user = user)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requestd for OpenID="' + form.openid.data + '",remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+    title = 'Sign In',
+    form = form,
+    providers = app.config['OPENID_PROVIDERS']
+    )
+
+
